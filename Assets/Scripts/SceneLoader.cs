c2eda8e2b2da83d6992aff7c1983f5cfdc7a8d8b
ashley06leddy <ashley06leddy@gmail.com>
2024-10-01 21:58:41 -0400

    added HUD to game
    Added UI Toolkit to my game scene; Vignette and 2 counters.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-   public void PlayGame()
+
+     public GameObject launchFirstButton;
+
+     // Start is called before the first frame
+     void Start()
+     {
+          // Clear selected object
+          EventSystem.current.SetSelectedGameObject(null);
+          // Set a new selected object
+          EventSystem.current.SetSelectedGameObject(launchFirstButton);
+
+     }
+     
+     public void PlayGame()
    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
    }
