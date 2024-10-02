0b03d45514a26ee2b5639402e1d737f73162b8a4
ashley06leddy <ashley06leddy@gmail.com>
2024-10-01 20:39:00 -0400

    Start button works
    Start button now starts the game.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-   public void QuitGame() 
+   public void PlayGame()
+   {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
+   
+   public void QuitGame()
    {
         Application.Quit();
         Debug.Log("Quit!");
-    }
+   }
 }
